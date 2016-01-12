323e5cd131ecb59d64336f275061dd14d5767fa0
Jon Borgonia <jon@gomagames.com>
2016-01-11 16:03:49 -1000

    handling mouse events and sprite animation

--- source/sprites/Square.hx
+++ source/sprites/Square.hx
@@ -1,5 +1,6 @@
 package sprites;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 using flixel.util.FlxSpriteUtil;
@@ -9,6 +10,9 @@ class Square extends FlxSprite{
   private static inline var rectangle_size : Float = 150;
   private static inline var rectangle_corner_radius : Float = 10;
 
+  public var hovering : Bool;
+  public var pressed : Bool;
+
   public function new()
   {
     // must call super constructor before making graphic
@@ -16,16 +20,38 @@ class Square extends FlxSprite{
 
     // use chaining to setup the shape graphics
     this.makeGraphic( rectangle_size, rectangle_size, FlxColor.TRANSPARENT )
-        .drawRoundRect( 0, 0, rectangle_size, rectangle_size, rectangle_corner_radius, rectangle_corner_radius, FlxColor.FOREST_GREEN, null, null, null);
+        .drawRoundRect( 0, 0, rectangle_size, rectangle_size, rectangle_corner_radius, rectangle_corner_radius, FlxColor.WHITE, null, null, null);
   }
 
   override public function update():Void
   {
     super.update();
+
+    handleInput();
+
+    handleAnimations();
+
   }
 
   override public function destroy():Void
   {
     super.destroy();
+  }
+
+  private inline function handleInput():Void
+  {
+    hovering = this.overlapsPoint(FlxG.mouse.getScreenPosition());
+    pressed = hovering && FlxG.mouse.pressed;
+  }
+
+  private inline function handleAnimations():Void
+  {
+    if(pressed){
+      this.set_color(FlxColor.HOT_PINK);
+    } else if(hovering){
+      this.set_color(FlxColor.CHARTREUSE);
+    } else {
+      this.set_color(FlxColor.FOREST_GREEN);
+    }
   }
 }
